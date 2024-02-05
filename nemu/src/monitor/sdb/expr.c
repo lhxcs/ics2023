917444b43c932715c2763eddebb944bb0d4730b1
tracer-ics2023 <tracer@njuics.org>
2024-02-05 21:57:41 +0800

    >  compile NEMU 3220106039 Hanxuan Li Linux Petrichor 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:57:41 up  7:11,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -211,6 +211,6 @@ word_t expr(char *e, bool *success) {
     return 0;
   }
   /* TODO: Insert codes to evaluate the expression. */
-  return (unsigned int) eval (0,nr_token - 1);
+  return eval (0,nr_token - 1);
 
 }
