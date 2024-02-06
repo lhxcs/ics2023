25bde7495f34021fcb84cbe2dfec38ca8ce5df4f
tracer-ics2023 <tracer@njuics.org>
2024-02-06 16:02:42 +0800

    >  compile NEMU 3220106039 Hanxuan Li Linux Petrichor 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:02:42 up 31 min,  1 user,  load average: 0.03, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -110,7 +110,8 @@ static bool make_token(char *e) {
 
         //tokens[nr_token].type = rules[i].token_type;
         switch (rules[i].token_type) {
-          case TK_NOTYPE: break;
+          case TK_NOTYPE:
+            break;
           case TK_NUM:
             tokens[nr_token].type = rules[i].token_type;
             strncpy(tokens[nr_token].str, substr_start, substr_len);
