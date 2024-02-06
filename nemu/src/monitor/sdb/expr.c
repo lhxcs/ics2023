80fa1ec5bd5bddca5769a661567887869267cbd5
tracer-ics2023 <tracer@njuics.org>
2024-02-06 15:51:02 +0800

    >  compile NEMU 3220106039 Hanxuan Li Linux Petrichor 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:51:02 up 19 min,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -110,8 +110,7 @@ static bool make_token(char *e) {
 
         //tokens[nr_token].type = rules[i].token_type;
         switch (rules[i].token_type) {
-          case TK_NOTYPE:
-            break;
+          
           case TK_NUM:
             strncpy(tokens[nr_token].str, substr_start, substr_len);
             tokens[nr_token].str[substr_len] = '\0';
@@ -126,22 +125,21 @@ static bool make_token(char *e) {
             break;
           default:
             tokens[nr_token].type = rules[i].token_type;
-            printf("%d %s\n",tokens[nr_token].type,tokens[nr_token].str);
+            //printf("%d %s\n",tokens[nr_token].type,tokens[nr_token].str);
             nr_token++;
         }
         break;
       }
     }
 
-    // for(int j = 0; j < nr_token; j++) {
-    //   if(tokens[j].type == '-' && ( (j == 0) || (tokens[j-1].type !=')' && tokens[j-1].type != TK_NUM && tokens[j-1].type != TK_HEX && tokens[j-1].type != TK_REG))) {
-    //     tokens[j].type = TK_NEG;
-    //   }
-    //   if(tokens[j].type == '-' && ( (j == 0) || (tokens[j-1].type !=')' && tokens[j-1].type != TK_NUM && tokens[j-1].type != TK_HEX && tokens[j-1].type != TK_REG))) {
-    //     tokens[j].type = TK_DEREF;
-    //   }
-
-    // }
+    for(int j = 0; j < nr_token; j++) {
+      if(tokens[j].type == '-' && ( (j == 0) || (tokens[j-1].type !=')' && tokens[j-1].type != TK_NUM && tokens[j-1].type != TK_HEX && tokens[j-1].type != TK_REG))) {
+        tokens[j].type = TK_NEG;
+      }
+      if(tokens[j].type == '-' && ( (j == 0) || (tokens[j-1].type !=')' && tokens[j-1].type != TK_NUM && tokens[j-1].type != TK_HEX && tokens[j-1].type != TK_REG))) {
+        tokens[j].type = TK_DEREF;
+      }
+    }
 
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
